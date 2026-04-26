cdb1db582813f77c797adf6cab1873a0749af340
Ifhdhd <108957265+Ifhdhd@users.noreply.github.com>
2026-04-27 00:19:03 +0700

    Refactor login function to use crypto for hashing

--- services/loginService.js
+++ services/loginService.js
@@ -1,50 +1,51 @@
 const axios = require("axios");
-const md5 = require("../utils/md5");
+const crypto = require("crypto");
+
+function md5(text) {
+  return crypto.createHash("md5").update(text).digest("hex");
+}
 
 async function login(account, password) {
-    const hashedPwd = md5(password);
+  try {
+    const response = await axios.post(
+      "https://ez-co-app.tin.group/app/offline/user/login",
+      {
+        account: account,
+        pwd: md5(password),
+        appVersion: "0"
+      },
+      {
+        headers: {
+          "X-COUNTRY-ID": "-1",
+          "countryCode": "ID",
+          "timeZoneId": "Asia/Jakarta",
+          "country": "ID",
+          "Accept-Language": "in-ID",
+          "deviceId": "ffffffff-a665-1a66-0000-0000748ca5f0",
+          "deviceModel": "5030U",
+          "osVersion": "10",
+          "versionCode": "300",
+          "versionName": "2.7.9-release",
+          "Content-Type": "application/json; charset=UTF-8",
+          "User-Agent": "okhttp/4.9.2"
+        }
+      }
+    );
 
-    try {
-        const response = await axios.post(
-            "https://ez-co-app.tin.group/app/offline/user/login",
-            {
-                account: account,
-                pwd: hashedPwd,
-                appVersion: "0"
-            },
-            {
-                headers: {
-                    "Content-Type": "application/json; charset=UTF-8",
-                    "X-COUNTRY-ID": "-1",
-                    "countryCode": "ID",
-                    "timeZoneId": "Asia/Jakarta",
-                    "country": "ID",
-                    "Accept-Language": "in-ID",
-                    "deviceId": "ffffffff-a665-1a66-0000-0000748ca5f0",
-                    "deviceModel": "5030U",
-                    "osVersion": "10",
-                    "versionCode": "300",
-                    "versionName": "2.7.9-release",
-                    "Accept-Encoding": "gzip",
-                    "Connection": "Keep-Alive",
-                    "User-Agent": "okhttp/4.9.2"
-                },
-                timeout: 15000
-            }
-        );
+    const cookies = response.headers["set-cookie"] || [];
 
-        return {
-            success: true,
-            data: response.data,
-            cookies: response.headers["set-cookie"] || []
-        };
+    return {
+      success: true,
+      data: response.data,
+      cookies: cookies
+    };
 
-    } catch (err) {
-        return {
-            success: false,
-            error: err.response?.data || err.message
-        };
-    }
+  } catch (error) {
+    return {
+      success: false,
+      error: error.message
+    };
+  }
 }
 
 module.exports = { login };
