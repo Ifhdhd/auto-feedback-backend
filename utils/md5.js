e125788d46e0ce1ed59e36b41b1f71c9791c5d5d
Ifhdhd <108957265+Ifhdhd@users.noreply.github.com>
2026-04-27 22:58:53 +0700

    Refactor md5 function to a single line export

--- utils/md5.js
+++ utils/md5.js
@@ -1,10 +1,5 @@
 const crypto = require("crypto");
 
-function md5(text) {
-  return crypto
-    .createHash("md5")
-    .update(text, "utf8")
-    .digest("hex");
-}
-
-module.exports = md5;
+module.exports = function md5(text) {
+  return crypto.createHash("md5").update(text).digest("hex");
+};
