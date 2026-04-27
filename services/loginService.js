ba84249595c6019a20a5af1b13a01f3d10057e3b
Ifhdhd <108957265+Ifhdhd@users.noreply.github.com>
2026-04-27 17:06:35 +0700

    Add login function to loginService
    Implement login function to authenticate users with hashed passwords and handle responses.

---
+++ services/loginService.js
@@ -0,0 +1,48 @@
+const axios = require("axios");
+const md5 = require("../utils/md5");
+
+async function login(account, password) {
+  try {
+    const hashedPwd = md5(password);
+
+    const response = await axios.post(
+      "https://ez-co-app.tin.group/app/offline/user/login",
+      {
+        account: account,
+        pwd: hashedPwd,
+        appVersion: "0"
+      },
+      {
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8",
+          "X-COUNTRY-ID": "-1",
+          "countryCode": "ID",
+          "timeZoneId": "Asia/Jakarta",
+          "country": "ID",
+          "Accept-Language": "in-ID",
+          "deviceId": "ffffffff-a665-1a66-0000-0000748ca5f0",
+          "deviceModel": "5030U",
+          "osVersion": "10",
+          "versionCode": "300",
+          "versionName": "2.7.9-release",
+          "User-Agent": "okhttp/4.9.2"
+        },
+        validateStatus: () => true
+      }
+    );
+
+    return {
+      success: true,
+      data: response.data,
+      cookies: response.headers["set-cookie"] || []
+    };
+
+  } catch (error) {
+    return {
+      success: false,
+      error: error.message
+    };
+  }
+}
+
+module.exports = { login };
