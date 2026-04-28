354933cc7035d03ded7307fdfafcd2816bb27041
Ifhdhd <108957265+Ifhdhd@users.noreply.github.com>
2026-04-29 00:17:04 +0700

    Remove data route and update server listening log

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const app = express();
 app.use(express.json());
 
 const auth = require("./routes/auth");
-const data = require("./routes/data");
-
 app.use("/api", auth);
-app.use("/api", data);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("🚀 jalan", PORT));
+app.listen(3000, () => {
+  console.log("jalan");
+});
