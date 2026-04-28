e6d36bec2a665c3ebeba69a1d5a37dfc84cfb657
Ifhdhd <108957265+Ifhdhd@users.noreply.github.com>
2026-04-28 23:43:29 +0700

    Update loginService.js

--- services/loginService.js
+++ services/loginService.js
@@ -4,7 +4,7 @@ const md5 = require("md5");
 async function login(account, password) {
   const hashed = md5(password);
 
-  const response = await axios.post(
+  const res = await axios.post(
     "https://ez-co-app.tin.group/app/offline/user/login",
     {
       account,
@@ -14,24 +14,15 @@ async function login(account, password) {
     {
       headers: {
         "Content-Type": "application/json; charset=UTF-8",
-        "User-Agent": "okhttp/4.9.2",
-        "X-COUNTRY-ID": "1",
-        "countryCode": "ID",
-        "timeZoneId": "Asia/Jakarta",
-        "country": "ID",
-        "Accept-Language": "in-ID",
-        "deviceId": "ffffffff-a665-1a66-0000-0000748ca5f0",
-        "deviceModel": "5030U",
-        "osVersion": "10",
-        "versionCode": "300",
-        "versionName": "2.7.9-release"
-      }
+        "User-Agent": "okhttp/4.9.2"
+      },
+      timeout: 20000
     }
   );
 
   return {
-    data: response.data,
-    cookies: response.headers["set-cookie"] || []
+    data: res.data,
+    cookies: res.headers["set-cookie"] || []
   };
 }
 
