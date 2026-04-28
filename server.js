0ecdd72a0693e6cca6eca395c09c18b3512f6d8b
Ifhdhd <108957265+Ifhdhd@users.noreply.github.com>
2026-04-28 14:02:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 
-const authRoutes = require("./routes/auth");
-const dataRoutes = require("./routes/data");
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", authRoutes);
-app.use("/api", dataRoutes);
+app.use("/api", require("./routes/auth"));
+app.use("/api", require("./routes/data"));
 
-app.listen(3000, () => {
-  console.log("🚀 Server jalan di http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("🚀 Server jalan:", PORT);
 });
