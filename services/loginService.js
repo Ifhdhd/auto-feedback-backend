7d20f0ade230611b1196dc49f0b31187be57792c
Ifhdhd <108957265+Ifhdhd@users.noreply.github.com>
2026-04-28 22:22:37 +0700

    Refactor loginService function to login

--- services/loginService.js
+++ services/loginService.js
@@ -1,52 +1,40 @@
 const axios = require("axios");
 const md5 = require("md5");
 
-async function loginService(account, password) {
-  try {
-    const hashed = md5(password);
+async function login(account, password) {
+  const hashed = md5(password);
 
-    const response = await axios.post(
-      "https://ez-co-app.tin.group/app/offline/user/login",
-      {
-        account,
-        pwd: hashed,
-        appVersion: "0"
-      },
-      {
-        headers: {
-          "Content-Type": "application/json; charset=UTF-8",
-          "User-Agent": "okhttp/4.9.2",
-
-          "X-COUNTRY-ID": "1",
-          "countryCode": "ID",
-          "timeZoneId": "Asia/Jakarta",
-          "country": "ID",
-          "Accept-Language": "in-ID",
-
-          "deviceId": "ffffffff-a665-1a66-0000-0000748ca5f0",
-          "deviceModel": "5030U",
-          "osVersion": "10",
-          "versionCode": "300",
-          "versionName": "2.7.9-release"
-        },
-        timeout: 20000
+  const response = await axios.post(
+    "https://ez-co-app.tin.group/app/offline/user/login",
+    {
+      account,
+      pwd: hashed,
+      appVersion: "0"
+    },
+    {
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+        "User-Agent": "okhttp/4.9.2",
+        "X-COUNTRY-ID": "1",
+        "countryCode": "ID",
+        "timeZoneId": "Asia/Jakarta",
+        "country": "ID",
+        "Accept-Language": "in-ID",
+        "deviceId": "ffffffff-a665-1a66-0000-0000748ca5f0",
+        "deviceModel": "5030U",
+        "osVersion": "10",
+        "versionCode": "300",
+        "versionName": "2.7.9-release"
       }
-    );
+    }
+  );
 
-    const cookies = response.headers["set-cookie"] || [];
+  const cookies = response.headers["set-cookie"] || [];
 
-    return {
-      success: true,
-      data: response.data,
-      cookies
-    };
-
-  } catch (err) {
-    return {
-      success: false,
-      error: err.response?.data || err.message
-    };
-  }
+  return {
+    data: response.data,
+    cookies
+  };
 }
 
-module.exports = { loginService };
+module.exports = { login };
