707ef436901be77fd9b9b5fbc4ac423af5f29776
Ifhdhd <108957265+Ifhdhd@users.noreply.github.com>
2026-04-29 00:24:56 +0700

    Refactor login function to use hashed password

--- services/loginService.js
+++ services/loginService.js
@@ -2,25 +2,40 @@ const axios = require("axios");
 const md5 = require("md5");
 
 async function login(account, password) {
-  const res = await axios.post(
+  const hashed = md5(password);
+
+  const response = await axios.post(
     "https://ez-co-app.tin.group/app/offline/user/login",
     {
       account,
-      pwd: md5(password),
+      pwd: hashed,
       appVersion: "0"
     },
     {
       headers: {
         "Content-Type": "application/json; charset=UTF-8",
-        "User-Agent": "okhttp/4.9.2"
-      }
+        "User-Agent": "okhttp/4.9.2",
+
+        "X-COUNTRY-ID": "1",
+        "countryCode": "ID",
+        "timeZoneId": "Asia/Jakarta",
+        "country": "ID",
+        "Accept-Language": "in-ID",
+
+        "deviceId": "ffffffff-a665-1a66-0000-0000748ca5f0",
+        "deviceModel": "5030U",
+        "osVersion": "10",
+        "versionCode": "300",
+        "versionName": "2.7.9-release"
+      },
+      timeout: 20000
     }
   );
 
   return {
-    data: res.data,
-    cookies: res.headers["set-cookie"] || []
+    data: response.data,
+    cookies: response.headers["set-cookie"] || []
   };
 }
 
-module.exports = loginService; // ⚠️ PENTING (bukan object)
+module.exports = { login };
