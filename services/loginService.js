9f905b7aa363b98d785d3467f4c15fa85b449e9c
Ifhdhd <108957265+Ifhdhd@users.noreply.github.com>
2026-04-28 13:30:34 +0700

    Refactor loginService.js headers and cookie handling

--- services/loginService.js
+++ services/loginService.js
@@ -14,37 +14,27 @@ async function login(account, password) {
       },
       {
         headers: {
-          "Content-Type": "application/json; charset=UTF-8",
+          "Content-Type": "application/json",
           "X-COUNTRY-ID": "1",
           "countryCode": "ID",
           "timeZoneId": "Asia/Jakarta",
-          "country": "ID",
-          "Accept-Language": "in-ID",
-          "deviceId": "ffffffff-a665-1a66-0000-0000748ca5f0",
-          "deviceModel": "5030U",
-          "osVersion": "10",
-          "versionCode": "300",
-          "versionName": "2.7.9-release",
-          "User-Agent": "okhttp/4.9.2"
+          "User-Agent": "okhttp/4.9.2",
+          "Cookie": "acw_tc=xxxxxxxx" // 🔥 WAJIB (isi dari browser)
         },
         validateStatus: () => true
       }
     );
 
-    const rawCookies = response.headers["set-cookie"] || [];
-
-    const cookies = rawCookies.map(c => c.split(";")[0]);
-
     return {
       success: true,
-      cookies,
-      data: response.data
+      data: response.data,
+      cookies: response.headers["set-cookie"] || []
     };
 
-  } catch (error) {
+  } catch (err) {
     return {
       success: false,
-      error: error.message
+      error: err.message
     };
   }
 }
