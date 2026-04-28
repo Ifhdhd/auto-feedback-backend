340b7cc8465670f853048749ed83697c71a1c6d4
Ifhdhd <108957265+Ifhdhd@users.noreply.github.com>
2026-04-28 20:28:37 +0700

    Update loginService.js

--- services/loginService.js
+++ services/loginService.js
@@ -1,46 +1,43 @@
 const axios = require("axios");
-const md5 = require("../utils/md5");
+const md5 = require("md5");
 
 async function login(account, password) {
-  try {
-    const res = await axios.post(
-      "https://ez-co-app.tin.group/app/offline/user/login",
-      {
-        account,
-        pwd: md5(password),
-        appVersion: "0"
+  const hashed = md5(password);
+
+  const response = await axios.post(
+    "https://ez-co-app.tin.group/app/offline/user/login",
+    {
+      account: account,
+      pwd: hashed,
+      appVersion: "0"
+    },
+    {
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+        "User-Agent": "okhttp/4.9.2",
+
+        "X-COUNTRY-ID": "1",
+        "countryCode": "ID",
+        "timeZoneId": "Asia/Jakarta",
+        "country": "ID",
+        "Accept-Language": "in-ID",
+
+        "deviceId": "ffffffff-a665-1a66-0000-0000748ca5f0",
+        "deviceModel": "5030U",
+        "osVersion": "10",
+        "versionCode": "300",
+        "versionName": "2.7.9-release"
       },
-      {
-        headers: {
-          "Content-Type": "application/json; charset=UTF-8",
-          "X-COUNTRY-ID": "1",
-          "countryCode": "ID",
-          "timeZoneId": "Asia/Jakarta",
-          "country": "ID",
-          "Accept-Language": "in-ID",
-          "deviceId": "ffffffff-a665-1a66-0000-0000748ca5f0",
-          "deviceModel": "5030U",
-          "osVersion": "10", // ✅ INI YANG ERROR TADI
-          "versionCode": "300",
-          "versionName": "2.7.9-release",
-          "User-Agent": "okhttp/4.9.2"
-        },
-        validateStatus: () => true
-      }
-    );
+      timeout: 20000
+    }
+  );
 
-    return {
-      success: true,
-      data: res.data,
-      cookies: res.headers["set-cookie"] || []
-    };
+  const cookies = response.headers["set-cookie"] || [];
 
-  } catch (err) {
-    return {
-      success: false,
-      error: err.message
-    };
-  }
+  return {
+    data: response.data,
+    cookies
+  };
 }
 
 module.exports = { login };
