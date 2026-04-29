ca626b99236c4918f867e5e96a0d7406dbbe5c3c
Ifhdhd <108957265+Ifhdhd@users.noreply.github.com>
2026-04-29 09:55:38 +0700

    Update loginService.js

--- services/loginService.js
+++ services/loginService.js
@@ -1,40 +1,28 @@
 const axios = require("axios");
 const md5 = require("md5");
 
-async function login(account, password) {
-  const hashed = md5(password);
-
-  const response = await axios.post(
+async function login(account, password){
+  const res = await axios.post(
     "https://ez-co-app.tin.group/app/offline/user/login",
     {
       account,
-      pwd: hashed,
-      appVersion: "0"
+      pwd: md5(password),
+      appVersion:"0"
     },
     {
-      headers: {
-        "Content-Type": "application/json; charset=UTF-8",
-        "User-Agent": "okhttp/4.9.2",
-
-        "X-COUNTRY-ID": "1",
-        "countryCode": "ID",
-        "timeZoneId": "Asia/Jakarta",
-        "country": "ID",
-        "Accept-Language": "in-ID",
-
-        "deviceId": "ffffffff-a665-1a66-0000-0000748ca5f0",
-        "deviceModel": "5030U",
-        "osVersion": "10",
-        "versionCode": "300",
-        "versionName": "2.7.9-release"
-      },
-      timeout: 20000
+      headers:{
+        "Content-Type":"application/json",
+        "User-Agent":"okhttp/4.9.2",
+        "X-COUNTRY-ID":"1",
+        "countryCode":"ID",
+        "timeZoneId":"Asia/Jakarta"
+      }
     }
   );
 
   return {
-    data: response.data,
-    cookies: response.headers["set-cookie"] || []
+    data: res.data,
+    cookies: res.headers["set-cookie"] || []
   };
 }
 
