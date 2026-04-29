45f0f40f078e2ecbf14e0ddb6f1b3e05d31e825b
Ifhdhd <108957265+Ifhdhd@users.noreply.github.com>
2026-04-29 09:53:19 +0700

    Refactor sessionStore to remove unnecessary string conversion

--- store/sessionStore.js
+++ store/sessionStore.js
@@ -1,19 +1,14 @@
 const sessions = new Map();
 
-function setSession(userId, cookies) {
-  sessions.set(String(userId), cookies);
+function setSession(userId, cookies){
+  sessions.set(userId, cookies);
 }
 
-function getSession(userId) {
-  return sessions.get(String(userId));
-}
-
-function deleteSession(userId) {
-  sessions.delete(String(userId));
+function getSession(userId){
+  return sessions.get(userId);
 }
 
 module.exports = {
   setSession,
-  getSession,
-  deleteSession
+  getSession
 };
