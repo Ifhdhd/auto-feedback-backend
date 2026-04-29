d48fafb0d6796898dbe63f174ddf70fdfd374975
Ifhdhd <108957265+Ifhdhd@users.noreply.github.com>
2026-04-29 09:52:48 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors()); // 🔥 INI WAJIB
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", require("./routes/auth"));
 app.use("/api", require("./routes/data"));
 
-app.listen(process.env.PORT || 3000);
+app.listen(3000, () => {
+  console.log("server jalan");
+});
