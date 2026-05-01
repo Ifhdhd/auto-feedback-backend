b4dbe0d4c58e2dfcdeb0ccd7f7845efa47fddbba
Ifhdhd <108957265+Ifhdhd@users.noreply.github.com>
2026-05-02 02:16:19 +0700

    Refactor login function for improved readability

--- services/loginService.js
+++ services/loginService.js
@@ -1,37 +1,79 @@
 const axios = require("axios");
 const md5 = require("md5");
 
-async function login(account, password, appVersion = "0") {
+async function login(
+  account,
+  password,
+  appVersion = "0"
+) {
+
+  const body = {
+
+    account,
+
+    pwd: md5(password),
+
+    appVersion: String(appVersion)
+
+  };
+
+  const headers = {
+
+    "Content-Type": "application/json",
+
+    "deviceId":
+      "ffffffff-a665-1a66-0000-0000748ca5f0",
+
+    "deviceModel":
+      "5030U",
+
+    "osVersion":
+      "10",
+
+    "versionCode":
+      "300",
+
+    "versionName":
+      "2.7.9-release",
+
+    "countryCode":
+      "ID",
+
+    "timeZoneId":
+      "Asia/Jakarta",
+
+    "User-Agent":
+      "okhttp/4.9.2"
+
+  };
 
   const res = await axios.post(
+
     "https://ez-co-app.tin.group/app/offline/user/login",
+
+    body,
+
     {
-      account,
-      pwd: md5(password),
-      appVersion: String(appVersion)
-    },
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "deviceId": "ffffffff-a665-1a66-0000-0000748ca5f0",
-        "deviceModel": "5030U",
-        "osVersion": "10",
-        "versionCode": "300",
-        "versionName": "2.7.9-release",
-        "countryCode": "ID",
-        "timeZoneId": "Asia/Jakarta"
-      }
+      headers
     }
+
   );
 
-  const cookies = res.headers["set-cookie"]
-    ?.map(c => c.split(";")[0])
-    .join("; ");
+  const cookies =
+    res.headers["set-cookie"]
+      ?.map(c => c.split(";")[0])
+      .join("; ") || "";
 
   return {
+
     data: res.data,
+
     cookies
+
   };
+
 }
 
-module.exports = { login };
+module.exports = {
+  login
+};
